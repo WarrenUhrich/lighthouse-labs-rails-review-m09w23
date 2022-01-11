badc497a25ce4391e89709b648fce7b34306ba68
Warren <hello@warren.codes>
2022-01-11 12:11:31 -0700

    Added our database seeds (character and location.)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,25 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+puts "Starting to seed the database..."
 
+puts "Generating locations..."
+
+30.times do
+    Location.create(
+        name: Faker::Movies::LordOfTheRings.location
+    )
+end
+
+locations = Location.all # An array of all locations.
+
+puts "Generating characters..."
+
+50.times do
+    Character.create(
+        name: Faker::Movies::LordOfTheRings.character,
+        location: locations.sample # Grabs one random item from the array.
+    )
+end
+
+puts "Seeding done."
